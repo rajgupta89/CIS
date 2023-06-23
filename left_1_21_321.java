9d4aa8f7c09ac3c9cd1881b801a1b1dbbec67b1a
rajgupta89 <gupta77raj123@gmail.com>
2023-06-23 16:29:23 +0530

    commit

--- left_1_21_321.java
+++ left_1_21_321.java
@@ -4,7 +4,7 @@ public class left_1_21_321 {
 		int n=5;
 		for (int i = 1; i <= n; i++) {
 			for (int j = i; j >= 1; j--) {
-				System.out.print(j);
+				System.out.print(j+" ");
 			}
 
 			System.out.println();
